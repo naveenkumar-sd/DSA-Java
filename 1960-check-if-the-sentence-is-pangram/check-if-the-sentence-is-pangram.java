39dfcddc22666e179fd7256f757ebf9b6d08d4f1
NaveenKumar <naveenkumarxk@gmail.com>
2025-08-09 09:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (79.74%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+    
+
+       if(sentence.length()<26) return false;
+        
+       for(char c='a';c<='z';c++){
+
+            if(!sentence.contains(String.valueOf(c))){
+                return false;
+            }
+
+       }
+
+       return true;
+       
+    }
+}
